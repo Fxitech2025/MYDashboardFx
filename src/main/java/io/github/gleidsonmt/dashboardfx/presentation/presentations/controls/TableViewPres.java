cbaeaa2ee1453d5c882703971af6f0ff18fd334f
Gleidson Neves da Silveira <gleidisonmt@gmail.com>
2025-04-12 13:50:25 -0300

    update table view pres

--- src/main/java/io/github/gleidsonmt/dashboardfx/presentation/presentations/controls/TableViewPres.java
+++ src/main/java/io/github/gleidsonmt/dashboardfx/presentation/presentations/controls/TableViewPres.java
@@ -3,11 +3,15 @@ package io.github.gleidsonmt.dashboardfx.presentation.presentations.controls;
 import io.github.gleidsonmt.dashboardfx.model.Person;
 import io.github.gleidsonmt.dashboardfx.presentation.internal.Tutorial;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
+
+import java.util.List;
 
 /**
  * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
@@ -24,70 +28,102 @@ public class TableViewPres extends StackPane {
                                 The TableView control is designed to visualize an unlimited number of rows of data, broken out into columns.
                                 A TableView is therefore very similar to the ListView control, with the addition of support for columns.""")
                         .legend("javafx.scene.control.TableView")
-                        .demo(createDemo())
                         .h5("Demo", "Table View")
+                        .demo(createDemo())
+
+                        .h3("Creating a Table View", "Table View")
+
+
+                        .text("""
+                                Creating a TableView is a multi-step process, and also depends on the underlying data model needing to be represented. 
+                                For this example we'll use an ObservableList<Person>, as it is the simplest way of showing data in a TableView. 
+                                The Person class will consist of a first name and last name properties. That is:
+                                """)
+
                         .code("""
                                 public class Person {
-                                    private String firstName = null;
-                                    private String lastName = null;
-                                
-                                    public Person() {
-                                    }
-                                
-                                    public Person(String firstName, String lastName) {
-                                        this.firstName = firstName;
-                                        this.lastName = lastName;
-                                    }
-                                
-                                    public String getFirstName() {
+                                    private StringProperty firstName;
+                                    public void setFirstName(String value) { firstNameProperty().set(value); }
+                                    public String getFirstName() { return firstNameProperty().get(); }
+                                    public StringProperty firstNameProperty() {
+                                        if (firstName == null) firstName = new SimpleStringProperty(this, "firstName");
                                         return firstName;
                                     }
                                 
-                                    public void setFirstName(String firstName) {
-                                        this.firstName = firstName;
-                                    }
-                                
-                                    public String getLastName() {
+                                    private StringProperty lastName;
+                                    public void setLastName(String value) { lastNameProperty().set(value); }
+                                    public String getLastName() { return lastNameProperty().get(); }
+                                    public StringProperty lastNameProperty() {
+                                        if (lastName == null) lastName = new SimpleStringProperty(this, "lastName");
                                         return lastName;
                                     }
                                 
-                                    public void setLastName(String lastName) {
-                                        this.lastName = lastName;
+                                    public Person(String firstName, String lastName) {
+                                        setFirstName(firstName);
+                                        setLastName(lastName);
                                     }
                                 }
-                                
-                                TableView<Person> tableView = new TableView<>();
-                                
-                                TableColumn<Person, String> columnOne = new TableColumn<>("Column One");
-                                columnOne.setCellValueFactory(new PropertyValueFactory<>("firstName"));
-                                TableColumn<Person, String> columnTwo = new TableColumn<>("Column Two");
-                                columnTwo.setCellValueFactory(new PropertyValueFactory<>("lastName"));
-                                
-                                tableView.getColumns().addAll(columnOne, columnTwo);
-                        
-                                tableView.setItems(FXCollections.observableArrayList(
-                                       new Person("Gleidson", "Neves da Silveira"),
-                                       new Person("Daniel", "Neves da Silveira")
-                                ));
                                 """)
+
+                        .text("The data we will use for this example is:")
+
+                        .code("""
+                                List<Person> members = List.of(
+                                    new Person("William", "Reed"),
+                                    new Person("James", "Michaelson"),
+                                    new Person("Julius", "Dean"));""")
+
+                        .text("""
+                                Firstly, we need to create a data model. As mentioned, for this example, we'll be using an ObservableList<Person>:""")
+                        .code("""
+                                ObservableList<Person> teamMembers = FXCollections.observableArrayList(members);""")
+                        .text("""
+                                Then we create a TableView instance:""")
+                        .code("""
+                                TableView<Person> table = new TableView<>();
+                                table.setItems(teamMembers);""")
+                        .text("""
+                                With the items set as such, TableView will automatically update whenever the teamMembers list changes. 
+                                If the items list is available before the TableView is instantiated, it is possible to pass it directly into the constructor:""")
+                        .code("""
+                                TableView<Person> table = new TableView<>(teamMembers);""")
+                        .text("""
+                                At this point we now have a TableView hooked up to observe the teamMembers observableList. 
+                                The missing ingredient now is the means of splitting out the data contained within the model and representing it in one or more TableColumn instances. 
+                                To create a two-column TableView to show the firstName and lastName properties, we extend the last code sample as follows:""")
+                        .code("""
+                                TableColumn<Person, String> firstNameCol = new TableColumn<>("First Name");
+                                firstNameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+                                TableColumn<Person, String> lastNameCol = new TableColumn<>("Last Name");
+                                lastNameCol.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+                                table.getColumns().setAll(firstNameCol, lastNameCol);""")
+
+                        .text("""
+                                With the code shown above we have fully defined the minimum properties required to create a TableView instance. 
+                                Running this code will result in the TableView being shown with two columns for firstName and lastName. 
+                                Any other properties of the Person class will not be shown, as no TableColumns are defined.""")
+                        .demo(createDemo())
                         .build().getRoot()
         );
     }
 
     private Node createDemo() {
+        List<Person> members = List.of(
+                new Person("William", "Reed"),
+                new Person("James", "Michaelson"),
+                new Person("Julius", "Dean"));
 
-        TableView<Person> tableView = new TableView<>();
-        TableColumn<Person, String> columnOne = new TableColumn<>("Column One");
-        columnOne.setCellValueFactory(new PropertyValueFactory<>("firstName"));
-        TableColumn<Person, String> columnTwo = new TableColumn<>("Column Two");
-        columnTwo.setCellValueFactory(new PropertyValueFactory<>("lastName"));
-        tableView.getColumns().addAll(columnOne, columnTwo);
+        ObservableList<Person> teamMembers = FXCollections.observableArrayList(members);
+        TableView<Person> table = new TableView<>();
+        table.setItems(teamMembers);
 
-        tableView.setItems(FXCollections.observableArrayList(
-               new Person("Gleidson", "Neves da Silveira"),
-               new Person("Daniel", "Neves da Silveira")
-        ));
-        return tableView;
+        TableColumn<Person, String> firstNameCol = new TableColumn<>("First Name");
+        firstNameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        TableColumn<Person, String> lastNameCol = new TableColumn<>("Last Name");
+        lastNameCol.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+
+        table.getColumns().setAll(firstNameCol, lastNameCol);
+        return table;
     }
 }
 
